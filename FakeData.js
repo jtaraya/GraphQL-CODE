10a1491f1a06e03b5121bcaec649d90d7a510c2a
Jacob Taraya <jacobtaraya@gmail.com>
2023-02-05 16:30:37 +0300

    initial commit resolvers

---
+++ FakeData.js
@@ -0,0 +1,40 @@
+const UserList = [
+    {
+        id: 1,
+        name: "Jabali",
+        username: "bali",
+        age: 1,
+        nationality: "Portugal",
+    },
+    {
+        id: 2,
+        name: "Jacob",
+        username: "capra",
+        age: 39,
+        nationality: "Kenya",
+    },
+    {
+        id: 3,
+        name: "Pedro",
+        username: "pedrotech",
+        age: 20,
+        nationality: "Brazil",
+    },
+    {
+        id: 4,
+        name: "Belinda",
+        username: "sonnie",
+        age: 35,
+        nationality: "Rwanda",
+    },
+    {
+        id: 5,
+        name: "Senteu",
+        username: "jason",
+        age: 6,
+        nationality: "Jamaica",
+    },
+
+];
+
+module.exports = { UserList };
