94a2b313fbe3536aabb458e5fe981daf0a926065
Jacob Taraya <jacobtaraya@gmail.com>
2023-02-08 19:30:18 +0300

    Mutation createUser: (parent, args) => {

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -37,9 +37,12 @@ const resolvers = {
     Mutation: {
         createUser: (parent, args) => {
             const user = args.input;
-            console.log(user);
-        }
-    }
+            const lastId = UserList[UserList.length-1].id;
+            user.id = lastId + 1;
+            UserList.push(user);
+            return user;
+        },
+    },
 };
 
 module.exports = { resolvers };
