cbaa83e5cddead15bf319d19c8aa26b0d97b15b8
Jacob Taraya <jacobtaraya@gmail.com>
2023-02-09 10:44:33 +0300

    npx create-react-app npm install @apollo/client establishing a connection with graphql api

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:3000/graphql" // this is where you can also  put a public api
+  })
+  return (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <h1>List Of Users</h1>
+      </div>
+    </ApolloProvider>
+  );
+  
+}
+
+export default App;
