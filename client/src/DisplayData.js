ea6de6c489925ab82139398639db63295cb738ed
Jacob Taraya <jacobtaraya@gmail.com>
2023-02-09 21:33:08 +0300

    UserMutation logic. input will update the state

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -2,119 +2,152 @@ import React, { useState } from "react";
 import { useQuery, useLazyQuery, gql } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
-    query GetAllUsers {
-        users {
-            id
-            name
-            age
-            username
-            nationality
-        }
+  query GetAllUsers {
+    users {
+      id
+      name
+      age
+      username
+      nationality
     }
-`
+  }
+`;
 
 const QUERY_ALL_MOVIES = gql`
-    query GetAllMovies {
-        movies {
-            id
-            name
-            yearOfRelease
-            isInTheaters
-        }
+  query GetAllMovies {
+    movies {
+      id
+      name
+      yearOfRelease
+      isInTheaters
     }
+  }
 `;
 
 const GET_MOVIE_BY_NAME = gql`
-query Movie($name: String!){
-    movie(name: $name){
-        name
-        yearOfRelease
+  query Movie($name: String!) {
+    movie(name: $name) {
+      name
+      yearOfRelease
     }
-}
+  }
 `;
 
 function DisplayData() {
+  const [movieSearched, setMovieSearched] = useState("");
 
-    const [movieSearched, setMovieSearched] = useState("")
+  // Create User States
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState("");
 
-    const {data, loading, error} = useQuery(QUERY_ALL_USERS);
-    const {data: movieData} = useQuery(QUERY_ALL_MOVIES);
-    const [fetchMovie,
-         {data: movieSearchedData, error: movieError }
-        ] = useLazyQuery(GET_MOVIE_BY_NAME);
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+  const [fetchMovie, { data: movieSearchedData, error: movieError }] =
+    useLazyQuery(GET_MOVIE_BY_NAME);
 
-    if (loading) {
-        return <h1> DATA IS LOADING...</h1>;
-    }
+  if (loading) {
+    return <h1> DATA IS LOADING...</h1>;
+  }
 
-    if(data) {
-        console.log(data);
-    }
+  if (data) {
+    console.log(data);
+  }
 
-    if (error) {
-        console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    if (movieError) {
-        console.log(movieError);
-    }
+  if (movieError) {
+    console.log(movieError);
+  }
 
-
-    return (
+  return (
     <div>
-        <div>
-            <input type="text" placeholder="Name..." />
-            <input type="text" placeholder="Username..." />
-            <input type="number" placeholder="Age..." />
-            <input type="text" placeholder="Nationality..." />
-            <button> Create User </button>
-        </div>
-        {data &&
-             data.users.map((user) => {
-                return (
-                    <div>
-                        <h1>Name: {user.name} </h1>
-                        <h1>Username: {user.username} </h1>
-                        <h1>Age: {user.age} </h1>
-                        <h1>Nationality: {user.nationality} </h1>
-                </div>
-            );
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Nationality..."
+          onChange={(event) => {
+            setNationality(event.target.value);
+          }}
+        />
+        <button> Create User </button>
+      </div>
+      {data &&
+        data.users.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name} </h1>
+              <h1>Username: {user.username} </h1>
+              <h1>Age: {user.age} </h1>
+              <h1>Nationality: {user.nationality} </h1>
+            </div>
+          );
         })}
 
-            {movieData &&
-                        movieData.movies.map((movie) => {
-                            return (
-                                <div>
-                                    <h1>Movie Name: {movie.name} </h1>
-                            </div>
-                        );
-                    })}
+      {movieData &&
+        movieData.movies.map((movie) => {
+          return (
+            <div>
+              <h1>Movie Name: {movie.name} </h1>
+            </div>
+          );
+        })}
 
-                    <div>
-                        <input
-                         type="text"
-                          placeholder="The Matrix..." 
-                          onChange= {(event) => {
-                            setMovieSearched(event.target.value)}} />
-                        <button 
-                        onClick={() => {fetchMovie({variables: {
-                            name: movieSearched,},
-                            });
-                        }}> 
-                        Fetch Data
-                        </button>
-                        <div>
-                            {movieSearchedData && (
-                                <div> 
-                                <h1> MovieName: {movieSearchedData.movie.name} </h1>
-                                <h1> yearOfRelease: {movieSearchedData.movie.yearOfRelease} </h1>
-                                </div>
-                                )}
-                                {movieError && <h1> There was an error fetching the data </h1>}
-                        </div>
-                    </div>
+      <div>
+        <input
+          type="text"
+          placeholder="The Matrix..."
+          onChange={(event) => {
+            setMovieSearched(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            fetchMovie({
+              variables: {
+                name: movieSearched,
+              },
+            });
+          }}
+        >
+          Fetch Data
+        </button>
+        <div>
+          {movieSearchedData && (
+            <div>
+              <h1> MovieName: {movieSearchedData.movie.name} </h1>
+              <h1> yearOfRelease: {movieSearchedData.movie.yearOfRelease} </h1>
+            </div>
+          )}
+          {movieError && <h1> There was an error fetching the data </h1>}
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default DisplayData;
