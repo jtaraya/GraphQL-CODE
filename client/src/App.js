769f7e357bd8b56aa89d25c2933084d0f71819f7
Jacob Taraya <jacobtaraya@gmail.com>
2023-02-09 10:49:01 +0300

    useQuery fetches data from an api=componentRenders

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, useQuery} from '@apollo/client';
 
 function App() {
   const client = new ApolloClient({
